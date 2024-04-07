14e87f4da1c3e675fec94b42822b1b9bb59b38b7
Venki1402 <141071215+Venki1402@users.noreply.github.com>
2024-04-07 17:59:55 +0530

    done till step-07

--- tests/step-03/index.test.js
+++ tests/step-03/index.test.js
@@ -1,11 +1,13 @@
 const parseQuery = require('../../src/queryParser');
 
+
+
 test('Parse SQL Query', () => {
     const query = 'SELECT id, name FROM sample';
     const parsed = parseQuery(query);
     expect(parsed).toEqual({
         fields: ['id', 'name'],
         table: 'sample',
-        whereClause: null
+        whereClauses: []
     });
 });
